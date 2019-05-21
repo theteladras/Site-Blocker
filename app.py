366238eeb661ff0865da3e4236bb4bec2382e206
Sanel Hadzini <sanel.hadzini@yahoo.com>
2019-05-21 12:32:07 +0200

    further functionalities

--- app.py
+++ app.py
@@ -1,33 +1,53 @@
 import time
 from datetime import datetime as date
+import services
+
 
 win_path = "C:\\Windows\\System32\\drivers\\etc"
 other_path = "/etc"
 test_path = "hosts"
-redirect = "127.0.0.1"
+work_begin_h = 9
+work_end_h = 17
+start_time = date(date.now().year, date.now().month,
+                  date.now().day, work_begin_h)
+end_time = date(date.now().year, date.now().month,
+                date.now().day, work_end_h)
+if services.hourCheck(start_time, end_time):
+    redirect = services.initial_setup()
 
-block_url = ["www.linkedin.com", "linkedin.com",
-             "www.youtube.com", "youtube.com"]
+block_url = [
+    "www.twitter.com", "twitter.com",
+    "www.youtube.com", "youtube.com",
+    "www.facebook.com", "facebook.com",
+    "www.instagram.com", "instagram.com"
+]
 
-itteration_num = 0
+print_once_switch = services.hourCheck(start_time, end_time)
 while True:
-    start_time = date(date.now().year, date.now().month, date.now().day, 9)
-    current_time = date.now()
-    end_time = date(date.now().year, date.now().month, date.now().day, 17)
-    if start_time < current_time < end_time:
-        print("Radno vreme")
-        if itteration_num == 0:
-            itteration_num = 1
+    if services.hourCheck(start_time, end_time):
+        print("\nRadno vreme...\n")
+        if print_once_switch:
+            print_once_switch = False
             with open(test_path, 'r+') as file:
                 content = file.read()
                 print(content)
                 for index, uri in enumerate(block_url):
                     if uri not in content:
                         if index == 0:
-                            file.write("\n" + redirect + " " + uri + "\n")
+                            flag = file.write(
+                                "\n" + redirect + " " + uri + "\n")
                         else:
                             file.write(redirect + " " + uri + "\n")
-                        print("added to the block list: ", uri)
+                        print("Added to the block list: ", uri)
     else:
-        print("Vreme odmora")
+        print("\nSada si slobodan!\n")
+        if not print_once_switch:
+            print_once_switch = True
+            with open(test_path, 'r+') as file:
+                content = file.readlines()
+                file.seek(0)
+                for line in content:
+                    if not any(website in line for website in block_url):
+                        file.write(line)
+                file.truncate()
     time.sleep(10)
---
+++ services.py
@@ -0,0 +1,47 @@
+from datetime import datetime as date
+
+
+def initial_setup():
+    input_not_in_order = True
+    while input_not_in_order:
+        print("Odaberite nacin redirekcije: \n")
+        print("1) ka -> www.google.com \n")
+        print("2) ka -> www.github.com \n")
+        print("3) ka -> www.codewars.com \n")
+        print("4) bez redirekcije \n")
+        print("5) unesite zeljenu adresu za redirekciju \n")
+        print("0) odustani \n")
+        redirect_page = int(input('Unesite broj opciju: '))
+        if (redirect_page > 4):
+            print("Niste uneli validnu opciju. Pokusajte ponovo.")
+        elif (redirect_page == 1):
+            redirect = "www.google.com"
+            input_not_in_order = False
+            return redirect
+        elif (redirect_page == 2):
+            redirect = "www.github.com"
+            input_not_in_order = False
+            return redirect
+        elif (redirect_page == 3):
+            redirect = "www.codewars.com"
+            input_not_in_order = False
+            return redirect
+        elif (redirect_page == 4):
+            redirect = "127.0.0.1"
+            input_not_in_order = False
+            return redirect
+        elif (redirect_page == 5):
+            input_not_in_order = False
+            chosen_url = input('Unesite zeljeni web domen: ')
+            redirect = chosen_url
+            return redirect
+        elif (redirect_page == 0):
+            exit()
+
+
+def hourCheck(start_time, end_time):
+    current_time = date.now()
+    if start_time < current_time < end_time:
+        return True
+    else:
+        return False
